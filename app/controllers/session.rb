97780a9e906b0d7a68180b83c042003ea1506815
Alan English & Dmitri & Joseph Connors <pair+alanenglish.imdmitri1.jcce@devbootcamp.com>
2017-07-27 15:59:59 -0500

    add login routes and form

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -1,10 +1,18 @@
+get '/session-viewer' do
+  session.inspect
+end
+
 get '/sessions/new' do
   erb :'session/new'
 end
 
 post '/sessions' do
-  @user = User.authenticate(params[:email], params[:password])
-  session[:user_id]=@user.id
-  session[:email]=@user.email
-  redirect "/"
+ @user = User.find_by(email: params[:email])
+  if @user && @user.authenticate(params[:password], params[:email])
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
+  else
+    @errors = ["There was trouble logging you in, please try again."]
+    erb :'/sessions/new'
+  end
 end

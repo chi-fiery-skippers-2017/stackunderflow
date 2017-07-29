9660ae7c2322729d63af2eae5bcc0f15de4c02e1
Alan English <email": null>
2017-07-29 17:08:51 -0500

    add comment capability to answers

--- app/controllers/answer.rb
+++ app/controllers/answer.rb
@@ -14,3 +14,18 @@ post '/questions/:id/answers/new' do
     erb :'questions/show'
   end
 end
+
+post '/answer/:id/comment' do
+  @answer = Answer.find_by(id: params[:id])
+  @comment = Comment.new(body: params[:comment], commentable_type: "Answer", commentable_id: params[:id], author_id: current_user.id)
+  if @comment.save
+    if request.xhr?
+      erb :'answers/_answer_comments', locals: { comment: @comment }, layout: false
+    else
+      redirect back
+    end
+  else
+    @errors = @answer.errors.full_messages
+    erb :'questions/show'
+  end
+end
--- public/js/application.js
+++ public/js/application.js
@@ -42,6 +42,71 @@ $(document).ready(function() {
     });
   });
 
+  //adding new comment
+  // $('.add_comment_button').on('click',function(){
+  //   event.preventDefault();
+  //   $(this).css('display', 'none');
+  //   $(this).parent().find('.add_comment_form').css('display', 'inline');
+  //   var $link = $(this).parent().find('.add_comment_form').attr('action');
+  //   var $info = $(this).parent().serialize();
+  //   console.log($info);
+  //   $.ajax({
+  //     url: $link,
+  //     method: "POST",
+  //     data: $info
+  //   }).done(function(response){
+  //     //insert new comment
+  //     // console.log($(this).closest('add_comments'));
+  //     console.log(response);
+  //     $('.post_comment_button').on('click', function(){
+  //       event.preventDefault();
+  //     })
+  //   })
+  // })
+
+  $(".add_comment_button").on("click", function(event){
+    event.preventDefault();
+    $(this).css('display', 'none');
+    $(this).parent().find('.add_comment_form').css('display', 'inline');
+  });
+
+  $(".add_comment_form").on("submit", function(event){
+    event.preventDefault();
+    var $this = $(this);
+    var answerComment = $(this).serialize();
+    var url = $this.closest('form').attr('action');
+    var type = $this.attr('method');
+
+    $.ajax({
+      data: answerComment,
+      url: url,
+      type: type
+    })
+
+    .done(function(response) {
+      $(".show_main_container").append(response);
+      console.log("success");
+      $this.css('display', 'none');
+      $this.parent().find('.add_comment_button').css('display', 'inline');
+      $this.children().first().val("")
+    })
+  })
+
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

1df8dfa3bf6bb42d738b0e19887af33840538527
JccE <toshajo99@gmail.com>
2017-07-31 00:21:54 -0500

    Subtle changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@ end
 
 20.times do
     Question.create(title: Faker::Book.title,
-                body: Faker::Hipster.sentence,
+                body: Faker::TwinPeaks.quote,
                 view_count: rand(1..100),
                 author_id: rand(1..10))
 end
 
 20.times do
-    Comment.create(body: Faker::Hipster.paragraph,
+    Comment.create(body: Faker::MostInterestingManInTheWorld.quote,
                   commentable_type: 'Question',
                   commentable_id: rand(1..20),
                   author_id: rand(1..10))
@@ -20,13 +20,13 @@ end
 
 
 30.times do
-    Answer.create(body: Faker::Hipster.sentence,
+    Answer.create(body: Faker::TwinPeaks.quote,
                   question_id: rand(1..20),
                   author_id: rand(1..10))
 end
 
 20.times do
-    Comment.create(body: Faker::Hipster.paragraph,
+    Comment.create(body: Faker::MostInterestingManInTheWorld.quote,
                   commentable_type: 'Answer',
                   commentable_id: rand(1..30),
                   author_id: rand(1..10))
